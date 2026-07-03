214b6100a9d95b9b986cbcf5b795908cb1c32f3b
Rouzax <GitHub@mgdn.nl>
2026-07-03 20:15:10 +0200

    Add myenergi Monitor plugin

--- plugin.py
+++ plugin.py
@@ -198,6 +198,7 @@ class BasePlugin:
             "MoonPhases":                   ["ycahome",         "MoonPhases",                           "Moon Phases",                       "master"],
             "Domoticz-MeshCore-Plugin":     ["galadril",        "Domoticz-MeshCore-Plugin",             "MeshCore",                          "main"],
             "MQTTDiscovery":                ["emontnemery",     "domoticz_mqtt_discovery",              "MQTT discovery",                    "master"],
+            "myenergi":                     ["Rouzax",          "myenergi-domoticz-plugin",             "myenergi zappi/harvi: solar, EV, home and grid energy with counters, plus opt-in control", "dist"],
             "Onkyo":                	    ["jorgh6",          "domoticz-onkyo-plugin",                "Onkyo AV Receiver",                 "master"],
             "owrtwifi2domo":                ["enesbcs",         "owrtwifi2domo",                        "OpenWRT WiFi Presence MQTT translator","master"],
             "pyrtl433":                     ["enesbcs",         "pyrtl433",                             "RTL_433 MQTT receiver",             "master"],
